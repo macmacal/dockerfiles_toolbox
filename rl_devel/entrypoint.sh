a7fe3b9c109d7f903f1e6ab5ae884b35b634028b
macmacal <mac.aleksandrowicz@gmail.com>
2023-05-07 11:24:29 +0200

    Fixed tensorboard run

--- rl_devel/entrypoint.sh
+++ rl_devel/entrypoint.sh
@@ -7,7 +7,7 @@ Xvfb $DISPLAY -screen 0 1200x720x24 +extension GLX +render -noreset &
 sleep 2
 
 echo ">>> Starting tensorboard"
-tensorboard --logdir=~/ray_results & > /dev/null
+tensorboard --logdir ~/ray_results --port 6006 --host 0.0.0.0 & > /dev/null
 
 echo ">>> Starting jupyter-lab"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/user/.mujoco/mujoco210/bin
